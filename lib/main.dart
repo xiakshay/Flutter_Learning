caf11ad80ca397401ab87d21a7632d4ef26925e6
Akshay Kumar Kushwaha <76196722+Akshay01-M@users.noreply.github.com>
2022-04-30 03:03:11 +0530

    Row Properties

--- lib/main.dart
+++ lib/main.dart
@@ -16,37 +16,34 @@ class HomePage extends StatelessWidget {
         title: const Text("Flutter App"),
       ),
       body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(15),
-          margin: const EdgeInsets.all(5),
-          alignment: Alignment.center,
-          height: 100,
-          width: 100,
-          // color: Color.fromARGB(255, 95, 212, 140), // if there is decoration then you can't use styling part out of the decoration element
-          decoration: BoxDecoration(
-              // color: Color.fromARGB(255, 95, 212, 140),
-              gradient: const LinearGradient(
-                colors: [Colors.yellow, Colors.red],
-              ), //gredient color to make it more colorful
-              border: Border.all(
-                  color: const Color.fromARGB(255, 97, 95, 212), width: 5),
-              borderRadius: BorderRadius.circular(10),
-              // ignore: prefer_const_literals_to_create_immutables
-              boxShadow: [
-                const BoxShadow(
-                  color: Color.fromARGB(255, 133, 133, 133),
-                  blurRadius: 10,
-                  offset: Offset(2, 10),
-                )
-              ]),
-          child: const Text(
-            "Box",
-            style: TextStyle(
-              color: Color.fromARGB(245, 255, 255, 255),
-              fontSize: 20,
-              fontWeight: FontWeight.w600,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(15),
+              margin: const EdgeInsets.all(5),
+              alignment: Alignment.center,
+              height: 100,
+              width: 100,
+              color: Colors.red,
             ),
-          ),
+            Container(
+              padding: const EdgeInsets.all(15),
+              margin: const EdgeInsets.all(5),
+              alignment: Alignment.center,
+              height: 100,
+              width: 100,
+              color: Colors.yellow,
+            ),
+            Container(
+              padding: const EdgeInsets.all(15),
+              margin: const EdgeInsets.all(5),
+              alignment: Alignment.center,
+              height: 100,
+              width: 100,
+              color: Colors.pink,
+            ),
+          ],
         ),
       ),
     );
