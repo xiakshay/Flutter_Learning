5be12bf48e570d6f710a43e0c249b9df4e4fcd56
Akshay Kumar Kushwaha <76196722+Akshay01-M@users.noreply.github.com>
2022-04-30 01:43:53 +0530

    Text Style customization

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     title: "Flutter App",
     home: HomePage(),
   ));
 }
 
 class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Flutter App"),
+        title: const Text("Flutter App"),
       ),
       body: Center(
         child: Container(
@@ -20,20 +22,31 @@ class HomePage extends StatelessWidget {
           alignment: Alignment.center,
           height: 100,
           width: 100,
-          child: Text("Box"),
           // color: Color.fromARGB(255, 95, 212, 140), // if there is decoration then you can't use styling part out of the decoration element
           decoration: BoxDecoration(
               // color: Color.fromARGB(255, 95, 212, 140),
-              gradient: LinearGradient(
+              gradient: const LinearGradient(
                 colors: [Colors.yellow, Colors.red],
               ), //gredient color to make it more colorful
-              border:
-                  Border.all(color: Color.fromARGB(255, 97, 95, 212), width: 5),
+              border: Border.all(
+                  color: const Color.fromARGB(255, 97, 95, 212), width: 5),
               borderRadius: BorderRadius.circular(10),
+              // ignore: prefer_const_literals_to_create_immutables
               boxShadow: [
-                BoxShadow(
-                    color: Color.fromARGB(255, 133, 133, 133), blurRadius: 10)
+                const BoxShadow(
+                  color: Color.fromARGB(255, 133, 133, 133),
+                  blurRadius: 10,
+                  offset: Offset(2, 10),
+                )
               ]),
+          child: const Text(
+            "Box",
+            style: TextStyle(
+              color: Color.fromARGB(245, 255, 255, 255),
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
         ),
       ),
     );
