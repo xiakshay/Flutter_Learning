80f129d1f749f0194df6b31a94bf8bf2003528f3
Akshay Kumar Kushwaha <76196722+Akshay01-M@users.noreply.github.com>
2022-04-30 01:13:07 +0530

    decoration element

--- lib/main.dart
+++ lib/main.dart
@@ -17,10 +17,15 @@ class HomePage extends StatelessWidget {
         child: Container(
           padding: const EdgeInsets.all(15),
           margin: const EdgeInsets.all(5),
-          color: Color.fromARGB(255, 95, 212, 140),
+          alignment: Alignment.center,
           height: 100,
           width: 100,
           child: Text("Hi! Developers"),
+          // color: Color.fromARGB(255, 95, 212, 140), // if there is decoration then you can't use styling part out of the decoration element
+          decoration: BoxDecoration(
+              color: Color.fromARGB(255, 95, 212, 140),
+              border: Border.all(
+                  color: Color.fromARGB(255, 97, 95, 212), width: 5)),
         ),
       ),
     );
