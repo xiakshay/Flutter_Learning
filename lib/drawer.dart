7e6e56121aafffbc52301a7b44ad503f15d60940
Akshay Kumar Kushwaha <76196722+Akshay01-M@users.noreply.github.com>
2022-05-06 03:37:14 +0530

    Drawer method in another class

---
+++ lib/drawer.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: dead_code
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: const <Widget>[
+          // DrawerHeader(
+          //   child: Text(
+          //     "Akshay Kumar",
+          //     style: TextStyle(
+          //       color: Colors.yellowAccent,
+          //       fontSize: 20,
+          //     ),
+          //   ),
+          //   decoration: BoxDecoration(color: Colors.indigo),
+          // ),
+          UserAccountsDrawerHeader(
+            accountName: Text("Akshay Kumar"),
+            accountEmail: Text("x0akshay@gmail.com"),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage:
+                  NetworkImage("https://wallpapercave.com/dwp1x/wp1865023.jpg"),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text("Account"),
+            subtitle: Text("Akshay"),
+            trailing: Icon(Icons.edit),
+          ),
+          ListTile(
+            leading: Icon(Icons.email),
+            title: Text("Email"),
+            subtitle: Text("x0akshay@gmail.com"),
+            trailing: Icon(Icons.edit),
+          ),
+          ListTile(
+            leading: Icon(Icons.book),
+            title: Text("Dockuments"),
+            subtitle: Text("Important Files"),
+          ),
+          ListTile(
+            leading: Icon(Icons.book),
+            title: Text("Dockuments"),
+            subtitle: Text("Important Files"),
+          )
+        ],
+      ),
+    );
+  }
+}
