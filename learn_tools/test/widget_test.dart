e562b3168def0aa90469e6e7dde1f56ec23d5944
Akshay Kumar Kushwaha <76196722+Akshay01-M@users.noreply.github.com>
2022-06-07 01:06:57 +0530

    previous app error solution

--- learn_tools/test/widget_test.dart
+++ learn_tools/test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:first_page/main.dart';
+// import 'package:learn_tools/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MaterialApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
